96d1e224044a17ab704d4084b0409600258bd1a4
Ljupcho Apostolov <lja@rackbeat.com>
2021-09-14 11:29:28 +0200

    Update WebhookBuilder.php

--- src/Builders/WebhookBuilder.php
+++ src/Builders/WebhookBuilder.php
@@ -7,12 +7,5 @@ class WebhookBuilder extends Builder
 {
     protected $entity = 'webhooks';
     protected $model  = Webhook::class;
-    protected $type = 'webhooks';
-    
-    public function delete(int $id)
-    {
-        return $this->request->handleWithExceptions( function () use ( $id ) {
-            $response = $this->request->client->delete( "{$this->entity}/{$id}" );
-        } );
-    }    
+    protected $type = 'webhooks';   
 }
